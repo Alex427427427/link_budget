a88236d976c079c95b490ea5d36931886878e5cc
Alexander Li <69694377+Alex427427427@users.noreply.github.com>
2025-04-30 15:49:10 +1000

    Update rain_plots.py

--- rain_plots.py
+++ rain_plots.py
@@ -1,6 +1,24 @@
 from L_rain import *
 import matplotlib.pyplot as plt
 import numpy as np
+import rain_rate as rr
+
+
+# plot the rain rate vs probability of excession at a given latitude
+lat_deg = 1.0
+lon_deg = 19.0
+rr_nom_array, p0_array = rr.rain_nominal_arrays(lat_deg, lon_deg)
+rr_t_array = np.linspace(0.0, 100.0, 100)
+px_ann_array = np.zeros(len(rr_t_array))
+for i, rr_t in enumerate(rr_t_array):
+    px_ann_array[i] = rr.px_annual(rr_t, p0_array, rr_nom_array)
+plt.plot(rr_t_array, px_ann_array*100, color='k')
+plt.xlabel("rain rate threshold [mm/h]")
+plt.ylabel("probability of excession [%]")
+plt.title(f"probability of exceeding rain rates at ({lat_deg:.2f}$^\\circ$,{lon_deg:.2f}$^\\circ$)")
+plt.savefig(f"plots\\probability of exceeding rain rates.png")
+plt.show()
+
 
 # to plot the rain attenuation coefficients for different frequencies 
 # and polarisation states
