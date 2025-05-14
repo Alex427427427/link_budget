c95c0191209673c796890df5f5e698d716c8ff89
Alexander Li <69694377+Alex427427427@users.noreply.github.com>
2025-05-14 16:00:43 +1000

    Update CN0_maps.py

--- CN0_maps.py
+++ CN0_maps.py
@@ -9,70 +9,109 @@ import numpy.ma as ma
 to produce an Eb/N0 map for the forward and return links for fixed ground station, varying user terminal. 
 adaptive modcod select.
 '''
-# constants
-C_FREE_SPACE = 299792458.0 # m/s
-R_GEO = 42164000 # m
-R_EQUATOR = 6378000 # m semi-major axis of earth
-R_POLAR = 6357000 # m semi-minor axis of earth
-ECCENTRICITY_EARTH = np.sqrt(1 - R_POLAR**2 / R_EQUATOR**2) # eccentricity of earth
-FLATTENING_EARTH = 1 - R_POLAR / R_EQUATOR
-K_DBW_per_HZ_K = -228.6 # Boltzmann's constant
+# beam frequency and bandwidth plans
+link_reliability = 0.995 # probability of link closure
+link_margin = 1.0 # dB (implementation loss)
+roll_off = 0.1 # ASSUMPTION
+SR_FORWARD = 52e6 # Mega symbols per second
+SR_RETURN = 10e6 # Mega symbols per second
+BW_ALLOCATED_FORWARD = SR_FORWARD*(1+roll_off) # Hz
+BW_ALLOCATED_RETURN = SR_RETURN*(1+roll_off) # Hz
+BW_FORWARD = 230.0e6 # Hz
+BW_RETURN = 220.0e6 # Hz
+OBO_FORWARD = 3.5 # dB
+OBO_RETURN = 5.8 # dB
+beam_plans = {
+    "fu": {
+        "17": 28.12e9, # L
+        "22": 28.12e9, # R
 
-# Communication specs
-BW = 230e6 # bandwidth in Hz
-BIT_RATE = 3e8 # bit rate in bps
-F_UP_FORWARD = 28.12e9 # uplink frequency in Hz
-F_DOWN_FORWARD = F_UP_FORWARD - 8.3e9 # downlink frequency in Hz
-F_UP_RETURN = 29.62e9 # uplink frequency in Hz
-F_DOWN_RETURN = F_UP_RETURN - 11.30e9 # downlink frequency in Hz
-link_reliability = 0.995
+        "21": 28.38e9, # L
+        "24": 28.38e9, # R
+
+        "25": 28.67e9, # R
+        "28": 28.67e9, # L
+
+        "26": 28.93e9, # L
+        "30": 28.93e9, # R
+    },
+    "fd": {
+        "22": 19.82e9, # L
+        "17": 19.82e9, # R
+
+        "24": 20.08e9, # L
+        "21": 20.08e9, # R
+
+        "28": 19.82e9, # R
+        "25": 19.82e9, # L
+
+        "30": 20.08e9, # L
+        "26": 20.08e9, # R
+    },
+    "ru": {
+        "17": 29.62e9, # L
+        "22": 29.62e9, # R
+
+        "21": 29.87e9, # L
+        "24": 29.87e9, # R
+
+        "25": 29.62e9, # R
+        "28": 29.62e9, # L
+
+        "26": 29.87e9, # L
+        "30": 29.87e9, # R
+    },
+    "rd": {
+        "22": 18.32e9, # L
+        "17": 18.32e9, # R
+
+        "24": 18.57e9, # L
+        "21": 18.57e9, # R
+
+        "28": 18.82e9, # R
+        "25": 18.82e9, # L
+
+        "30": 19.07e9, # L
+        "26": 19.07e9, # R
+    }
+}
 
 # gateway specs
 gate = {
-    'lat': 36, # degrees
-    'lon': 19, # degrees
+    'lat': 33.22, # degrees
+    'lon': 35.10, # degrees
     'alt': 0.0, # meters
-    'PSD': 57.5e-6, # dBW/Hz
-    'EIRP': 57.5e-6 + 10*np.log10(BW), # dBW
+    'PSD': 57.5 - 60, # dBW/Hz
+    'EIRP': 57.5 - 60 + 10*np.log10(BW_ALLOCATED_FORWARD), # dBW
     'G/T': 38.2, # dB/K
     'C/IM': 20.0 # dB
 }
 # user terminal specs
 uterm = {
-    'lat': 36, # degrees
-    'lon': 19, # degrees
+    'lat': 33.22, # degrees
+    'lon': 35.10, # degrees
     'alt': 0.3048 * 30000, # meters
     'EIRP': 58.5, # dBW
     'G/T': 15.0 # dB/K
 }
-EIRP_sat_f = kgq.highest_EIRP_query(uterm['lat'], uterm['lon']) # dBW
-GT_sat_f = kgq.highest_GT_query(gate['lat'], gate['lon']) # dB/K
-EIRP_sat_r = kgq.highest_EIRP_query(gate['lat'], gate['lon']) # dBW
-GT_sat_r = kgq.highest_GT_query(uterm['lat'], uterm['lon']) # dB/K
 # satellite specs
 sat = {
     'lat': 0, # degrees
     'lon': 31.0, # degrees
-    'alt': R_GEO - R_EQUATOR, # meters
-    'EIRP_f': EIRP_sat_f, # dBW
-    'G/T_f': GT_sat_f, # dB/K
-    'EIRP_r': EIRP_sat_r, # dBW
-    'G/T_r': GT_sat_r # dB/K
+    'alt': lb.R_GEO - lb.R_EQUATOR, # meters
+    'EIRP_f': 0.0, # dBW
+    'G/T_f': 0.0, # dB/K
+    'EIRP_r': 0.0, # dBW
+    'G/T_r': 0.0, # dB/K
 }
 
+
 # geographic coords of the user terminal
-ulat_list = np.linspace(-90, 90, 181) # degrees
-ulon_list = np.linspace(-180, 180, 361) # degrees
+ulat_list = np.linspace(-90, 90, 361) # degrees
+ulon_list = np.linspace(-180, 180, 721) # degrees
 CN0_fmap = np.zeros((len(ulat_list), len(ulon_list)))
 CN0_rmap = np.zeros((len(ulat_list), len(ulon_list)))
 
-'''
-for r, ulat in enumerate(ulat_list):
-    print("========================")
-    print(f"Calculating for LAT = {ulat}...")
-    for c, ulon in enumerate(ulon_list):
-        print(f"LON: {ulon}")
-    '''
 for c, ulon in enumerate(ulon_list):
     print("========================")
     print(f"Calculating for LON = {ulon}...")
@@ -80,13 +119,34 @@ for c, ulon in enumerate(ulon_list):
         
         uterm['lat'] = ulat
         uterm['lon'] = ulon
-        sat['EIRP_f'] = kgq.highest_EIRP_query(uterm['lat'], uterm['lon']) # dBW
-        sat['G/T_r'] = kgq.highest_GT_query(uterm['lat'], uterm['lon']) # dB/K
-
-        if sat['EIRP_f'] is None or sat['G/T_f'] is None or sat['EIRP_r'] is None or sat['G/T_r'] is None:
+        # satellite specs
+        EIRP_sat_f, beam_id_fd = kgq.highest_EIRP_query(uterm['lat'], uterm['lon']) # dBW
+        GT_sat_f, beam_id_fu = kgq.highest_GT_query(gate['lat'], gate['lon']) # dB/K
+        EIRP_sat_r, beam_id_rd = kgq.highest_EIRP_query(gate['lat'], gate['lon']) # dBW
+        GT_sat_r, beam_id_ru = kgq.highest_GT_query(uterm['lat'], uterm['lon']) # dB/K
+        if EIRP_sat_f is None or GT_sat_f is None or EIRP_sat_r is None or GT_sat_r is None:
+            # set the maps to -inf
             CN0_fmap[r, c] = -np.inf
             CN0_rmap[r, c] = -np.inf
             continue
+        # satellite specs
+        sat['EIRP_f'] = EIRP_sat_f - OBO_FORWARD + np.log10(BW_ALLOCATED_FORWARD/BW_FORWARD) # dBW
+        sat['G/T_f'] = GT_sat_f # dB/K
+        sat['EIRP_r'] = EIRP_sat_r - OBO_RETURN + np.log10(BW_ALLOCATED_RETURN/BW_RETURN)
+        sat['G/T_r'] = GT_sat_r
+
+        try: 
+            # Communication specs
+            F_UP_FORWARD = beam_plans["fu"][str(beam_id_fu)] # uplink frequency in Hz
+            F_DOWN_FORWARD = beam_plans["fd"][str(beam_id_fd)] # downlink frequency in Hz
+            F_UP_RETURN = beam_plans["ru"][str(beam_id_ru)] # uplink frequency in Hz
+            F_DOWN_RETURN = beam_plans["rd"][str(beam_id_rd)]# downlink frequency in Hz
+        except KeyError as e:
+            # set the maps to -inf
+            CN0_fmap[r, c] = -np.inf
+            CN0_rmap[r, c] = -np.inf
+            continue
+
         print(f"LAT: {ulat}")
 
         forward_results = lb.full_link_budget(
@@ -99,8 +159,8 @@ for c, ulon in enumerate(ulon_list):
             G_per_T_Earth_end=uterm['G/T'], # dB/K
             f_up=F_UP_FORWARD, # frequency in Hz
             f_down=F_DOWN_FORWARD, # frequency in Hz
-            BW=BW, # bandwidth in Hz
-            link_reliability=link_reliability, # link reliability
+            BW=BW_ALLOCATED_FORWARD, # bandwidth in Hz
+            link_reliability=link_reliability, # probability of link closure
             #bit_rate=BIT_RATE, # bit rate in bps
             C_IM=gate['C/IM'] # dB
         )
@@ -108,21 +168,24 @@ for c, ulon in enumerate(ulon_list):
         return_results = lb.full_link_budget(
             EIRP_Earth_start=uterm['EIRP'], # dBW
             Earth_start_lat_lon_alt=(np.radians(uterm['lat']), np.radians(uterm['lon']), uterm['alt']), # lat, lon, alt in radians and meters
+            
             sat_lat_lon_alt=(np.radians(sat['lat']), np.radians(sat['lon']), sat['alt']), # lat, lon, alt in radians and meters
             G_per_T_sat=sat['G/T_r'], # dB/K
             EIRP_sat=sat['EIRP_r'], # dBW
+            
             Earth_end_lat_lon_alt=(np.radians(gate['lat']), np.radians(gate['lon']), gate['alt']), # lat, lon, alt in radians and meters
             G_per_T_Earth_end=gate['G/T'], # dB/K
+            
             f_up=F_UP_RETURN, # frequency in Hz
             f_down=F_DOWN_RETURN, # frequency in Hz
-            BW=BW, # bandwidth in Hz
-            link_reliability=link_reliability, # link reliability
+            BW=BW_ALLOCATED_RETURN, # bandwidth in Hz
+            link_reliability=link_reliability, # probability of link closure
             #bit_rate=BIT_RATE, # bit rate in bps
             C_IM=gate['C/IM'] # dB
         )
 
-        CN0_fmap[r, c] = forward_results['CN0_dB']
-        CN0_rmap[r, c] = return_results['CN0_dB']
+        CN0_fmap[r, c] = forward_results['C/N0_dB']
+        CN0_rmap[r, c] = return_results['C/N0_dB']
 
 # save data
 np.savetxt('outputs\\CN0_fmap.csv', CN0_fmap, fmt='%.4f', delimiter=',')
